d4d7ffd6c3610625e3e5568f3ea10d7a3a96189e
Rebecca <beccadias9@gmail.com>
2026-05-14 16:33:13 -0300

    acrescenta estruturas de testes

--- Lacofor/MainWindow.xaml.cs
+++ Lacofor/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace LacoFor;
 /// </summary>
 public partial class MainWindow : Window
 {
-    public decimal saldoInicial = 160.00M;
+    public decimal saldoInicial = 12345.00M;
 
     public MainWindow()
     {
@@ -26,15 +26,32 @@ public partial class MainWindow : Window
 
     private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
     {
-        btnSorteio.IsEnabled = false;
         
         var quantidadeTexto = tbQuantidade.Text;
-        var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
-        if (quantidadeSorteios < 1)
+        int quantidadeSorteios;
+
+        try
+        {
+            quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
+        }
+        catch (FormatException)
+        {
+            MessageBox.Show("Entrada inválida. Coloque apenas numeros de 1 para cima!");
+            return;
+        }
+        catch (OverflowException)
+        {
+            MessageBox.Show("Erro! O numero digitado é maior que o numero suportado!");
+            return;
+        }
+
+        btnSorteio.IsEnabled = false;
+        if (quantidadeSorteios < 1) 
         {
             quantidadeSorteios = 1;
         }
 
+        btnSorteio.IsEnabled = false;
         var sorteador = new Random();
 
         for (int contador = 0; contador < quantidadeSorteios; contador++)
