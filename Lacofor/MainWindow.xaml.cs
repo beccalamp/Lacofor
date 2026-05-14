0a3de65e40e0379e9c95abb40ac15f4ca743d55d
Rebecca <beccadias9@gmail.com>
2026-05-14 15:08:27 -0300

    adiciona saldo dinamico e desativa botão

--- Lacofor/MainWindow.xaml.cs
+++ Lacofor/MainWindow.xaml.cs
@@ -16,29 +16,43 @@ namespace LacoFor;
 /// </summary>
 public partial class MainWindow : Window
 {
+    public decimal saldoInicial = 160.00M;
+
     public MainWindow()
     {
         InitializeComponent();
+        tbSaldo.Text = $"R$ {saldoInicial}";
     }
 
     private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
     {
+        btnSorteio.IsEnabled = false;
+        
         var quantidadeTexto = tbQuantidade.Text;
         var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
-        //var quantidadeSorteios = Convert.ToInt32(tbQuantidade.Text);
-
         if (quantidadeSorteios < 1)
         {
             quantidadeSorteios = 1;
         }
-        
+
         var sorteador = new Random();
-        // contador++ ; contador += 1; contador = contador + 1
+
         for (int contador = 0; contador < quantidadeSorteios; contador++)
         {
-            tbResultado.Text = sorteador.Next(0, 40000000).ToString();
-            await Task.Delay(1000);
+            if (saldoInicial >= 10)
+            {
+                tbResultado.Text = sorteador.Next(0, 4000000).ToString();
+                saldoInicial -= 10;
+                tbSaldo.Text = $"R$ {saldoInicial}";
+                await Task.Delay(1000);
+            }
+            else
+            {
+                MessageBox.Show("Você não tem saldo suficiente para realizar o sorteio!");
+                break;
+            }
         }
-        
+
+        btnSorteio.IsEnabled = true;
     }
 }
